978707607bd113da4c926885b98030834b26e907
Asko Nõmm <asko@nth.ee>
2024-06-27 00:36:45 +0300

    Add tests

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -22,6 +22,46 @@ class UserTest extends TestCase
     $this->assertEquals([1], $query->data());
   }
 
+  public function testAndWhereQuery(): void
+  {
+    $query = (new User)->query()->select('*')->where("id", "=", 1)->andWhere("name", "=", "John Doe");
+
+    $this->assertEquals("SELECT * FROM users WHERE id = ? AND name = ?", $query->sql());
+    $this->assertEquals([1, "John Doe"], $query->data());
+  }
+
+  public function testOrWhereQuery(): void
+  {
+    $query = (new User)->query()->select('*')->where("id", "=", 1)->orWhere("name", "=", "John Doe");
+
+    $this->assertEquals("SELECT * FROM users WHERE id = ? OR name = ?", $query->sql());
+    $this->assertEquals([1, "John Doe"], $query->data());
+  }
+
+  public function testOrderByQuery(): void
+  {
+    $query = (new User)->query()->select('*')->orderBy("name", "desc");
+
+    $this->assertEquals("SELECT * FROM users ORDER BY name desc", $query->sql());
+    $this->assertEquals([], $query->data());
+  }
+
+  public function testLimitQuery(): void
+  {
+    $query = (new User)->query()->select('*')->limit(10);
+
+    $this->assertEquals("SELECT * FROM users LIMIT 10", $query->sql());
+    $this->assertEquals([], $query->data());
+  }
+
+  public function testOffsetQuery(): void
+  {
+    $query = (new User)->query()->select('*')->offset(10);
+
+    $this->assertEquals("SELECT * FROM users OFFSET 10", $query->sql());
+    $this->assertEquals([], $query->data());
+  }
+
   public function testModelCreation(): void
   {
     $query = (new User)->query()->select('*')->where("id", "=", 1);
